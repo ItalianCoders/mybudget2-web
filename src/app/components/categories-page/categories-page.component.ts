10f1ecc1a0292b0edec699f97607e00fd97d4f6e
Gianluca Cazzaniga <gianluc95@hotmail.it>
2020-04-17 22:34:11 +0200

    Creato service per lettura e cancellazione categorie

--- src/app/components/categories-page/categories-page.component.ts
+++ src/app/components/categories-page/categories-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from 'src/app/services/category.service';
+import { Category } from 'src/app/models/category';
 
 @Component({
   selector: 'app-categories-page',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories-page.component.scss']
 })
 export class CategoriesPageComponent implements OnInit {
+  categories: Category[];
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit(): void {
+    this.categoryService.getCategories().subscribe(
+      categories => this.categories = categories
+    );
   }
 
+  deleteCategory(id: number) {
+    this.categoryService.deleteCategory(id).subscribe(
+      response => {
+        this.categories = this.categories.filter(category => category.id !== id);
+      }
+    );
+  }
 }
