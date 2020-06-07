dfba6c94a667f53d98cb997138c7358badaf0df9
Gianluca Cazzaniga <gianluc95@hotmail.it>
2020-06-07 19:21:03 +0200

    Creato nuovo componente per ultimi movimenti

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
-import { LastMovementsComponent } from 'src/app/components/last-movements/last-movements.component';
 import { CoreModule } from 'src/app/core/core.module';
 
 import { MatListModule } from '@angular/material/list';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatDividerModule } from '@angular/material/divider';
+import { LastMovementsComponent } from './components/last-movements/last-movements.component';
 
 
 @NgModule({
